354f352c31dd750fd81bb25086f79c75ef2e8970
j-hnn <john.weng@searcyschools.org>
2024-01-30 11:13:30 -0600

    finished checkout system

--- checkout.py
+++ checkout.py
@@ -16,3 +16,33 @@ def start(order):
     print("Subtotal: $" + str(subtotal))
     print("Tax: $" + str(tax))
     print("Total: $" + str(total))
+
+    payment(total)
+
+def payment(total):
+    while True:
+        payment_type = input("Cash or credit? ")
+        if payment_type.lower() == "cash":
+            print(f"The total is ${total}.")
+            cash = int(input("Enter cash received: "))
+            change = round(cash - total, 2)
+            print(f"Your change is ${change}.")
+            input("Press ENTER to continue")
+            break
+        elif payment_type.lower() == "credit":
+            print(f"The total is ${total}.")
+            print("Please swipe, insert, or tap.")
+            print("Transaction Complete")
+            input("Press ENTER to continue")
+            break
+        elif payment_type.lower() == "chuck e. cheese tokens":
+            chuck_total = round(total / 2)
+            print(f"The total is {chuck_total} tokens.")
+            tokens = int(input("Tokens received: "))
+            change = chuck_total - tokens
+            print(f"Your change is {change} tokens.")
+            input("Press ENTER to continue")
+            break
+        else:
+            print("Please input cash or credit only")
+            input("Press ENTER to continue")
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ Author: John Weng
 Version: 1.0
 """
 
-import order, checkout, inventory
+import order, checkout
+from os import system
 
 print("Wekcome to Pizza Time!")
 
@@ -14,8 +15,7 @@ while True:
     print("Select an option below.")
     print("1. Order")
     print("2. Checkout")
-    print("3. Inventory")
-    print("4. Exit")
+    print("3. Exit")
 
     selection = input(">> ")
     if selection == "1":
@@ -26,9 +26,10 @@ while True:
         else:
             print("The cart is empty")
     elif selection == "3":
-        inventory.start()
-    elif selection == "4":
         print("Goodbye!")
         break
     else:
         print("Please enter a valid selection")
+        input("Press ENTER to continue")
+
+    system("cls")
